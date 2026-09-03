eaeb4b7f008e64915ed5014359f80ccf73e631d1
cajub311 <69664397+cajub311@users.noreply.github.com>
2026-09-03 18:23:11 -0500

    Remove accidental places.js fragment

--- places.js
+++
@@ -1,5 +0,0 @@
-const PLACES={
-    tavern:{name:'The Ember & Ash',x:3,y:4,box:[1,1,4,3],color:'#754b3f'}, elders_hall:{name:"Elder's Hall",x:10,y:4,box:[8,1,4,3],color:'#66506d'}, chapel:{name:'Chapel of the Veil',x:16,y:4,box:[14,1,4,3],color:'#59687c'},
-    graveyard:{name:'The Graveyard',x:0,y:5,color:'#4e665b'}, square:{name:'Village Square',x:10,y:5,color:'#8d765a'}, cryptgate:{name:'The Crypt Gate',x:19,y:5,color:'#51384f'},
-    smithy:{name:"Hetta's Forge",x:3,y:6,box:[1,7,4,3],color:'#8a513c'}, market:{name:'Market Row',x:10,y:6,box:[8,7,4,3],color:'#6b6245'}, lantern:{name:'The Velvet Lantern',x:16,y:6,box:[14,7,4,3],color:'#71465f'}, wheatfields:{name:'Wheatfield Road',x:10,y:10,color:'#8c7545'}
-  };
